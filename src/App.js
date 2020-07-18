207b0bdada0f9284f29838533dfc5cb6476cdff2
pepeyen <erickfcaldeira@gmail.com>
2020-07-18 18:38:58 -0300

    Adding default create-react-app files while deleting unused files

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App__header">
+      </header>
+      <main className="App__main">
+      </main>
+      <footer className="App__footer">
+      </footer>
+    </div>
+  );
+}
+
+export default App;
