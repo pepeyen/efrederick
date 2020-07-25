1f7680c707a2b38db637cd2254f7af1940d71617
pepeyen <erickfcaldeira@gmail.com>
2020-07-25 00:10:22 -0300

    Updates

---
+++ precache-manifest.86eb03798b70c623afec62383c8553af.js
@@ -0,0 +1,130 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d93d9bad72900580e95af3aac23f76c5",
+    "url": "/efrederick/index.html"
+  },
+  {
+    "revision": "f3f0b31c41fbfe45adb7",
+    "url": "/efrederick/static/css/main.e60d027c.chunk.css"
+  },
+  {
+    "revision": "2f5d9da45732dea962ad",
+    "url": "/efrederick/static/js/2.c5340762.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/efrederick/static/js/2.c5340762.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f3f0b31c41fbfe45adb7",
+    "url": "/efrederick/static/js/main.f3a4c56d.chunk.js"
+  },
+  {
+    "revision": "281f51932976c2ff8db5",
+    "url": "/efrederick/static/js/runtime-main.94a0c0b8.js"
+  },
+  {
+    "revision": "4068b158a885483fabd4ebcdcfaa0861",
+    "url": "/efrederick/static/media/Nunito-Bold.4068b158.svg"
+  },
+  {
+    "revision": "91a2c2fd5cbb3ab5d56cb95fe1902263",
+    "url": "/efrederick/static/media/Nunito-Bold.91a2c2fd.ttf"
+  },
+  {
+    "revision": "b5b3aaf1afd832622d77384c55b93de9",
+    "url": "/efrederick/static/media/Nunito-Bold.b5b3aaf1.eot"
+  },
+  {
+    "revision": "cfa359daba0e6a141225e6bac1cae7aa",
+    "url": "/efrederick/static/media/Nunito-Bold.cfa359da.woff"
+  },
+  {
+    "revision": "04d5ab7e59dcb5d54423149857d0f352",
+    "url": "/efrederick/static/media/Nunito-ExtraBold.04d5ab7e.ttf"
+  },
+  {
+    "revision": "51cbd67a8b614065f11888d91735afc8",
+    "url": "/efrederick/static/media/Nunito-ExtraBold.51cbd67a.eot"
+  },
+  {
+    "revision": "75dd9eb31e59e7fd104564af30f0621d",
+    "url": "/efrederick/static/media/Nunito-ExtraBold.75dd9eb3.svg"
+  },
+  {
+    "revision": "9c2b863f6a64450ae5d14c607aeaa9f6",
+    "url": "/efrederick/static/media/Nunito-ExtraBold.9c2b863f.woff"
+  },
+  {
+    "revision": "558663b7a35fe3edf1d993837f90abd9",
+    "url": "/efrederick/static/media/Nunito-ExtraLight.558663b7.ttf"
+  },
+  {
+    "revision": "77cca7aa6c9a27521ce181ad25a34ced",
+    "url": "/efrederick/static/media/Nunito-ExtraLight.77cca7aa.svg"
+  },
+  {
+    "revision": "8b0a972ae1d5de8634efaaf6ad613d5b",
+    "url": "/efrederick/static/media/Nunito-ExtraLight.8b0a972a.eot"
+  },
+  {
+    "revision": "ec59fcd1e29a348c71009e9634063a05",
+    "url": "/efrederick/static/media/Nunito-ExtraLight.ec59fcd1.woff"
+  },
+  {
+    "revision": "45815d68d30a60dbe3bf66220ce4deb1",
+    "url": "/efrederick/static/media/Nunito-Light.45815d68.ttf"
+  },
+  {
+    "revision": "584947c9c1bd17dae874e679d7a141d4",
+    "url": "/efrederick/static/media/Nunito-Light.584947c9.eot"
+  },
+  {
+    "revision": "c3d94053856192732530569319daa8d1",
+    "url": "/efrederick/static/media/Nunito-Light.c3d94053.woff"
+  },
+  {
+    "revision": "f8afb2475e0eb6d6a9cdffa3fda795ef",
+    "url": "/efrederick/static/media/Nunito-Light.f8afb247.svg"
+  },
+  {
+    "revision": "0860ca1baff6e60f0e4a1190dfbe09c2",
+    "url": "/efrederick/static/media/Nunito-Regular.0860ca1b.eot"
+  },
+  {
+    "revision": "2759fcd401b357d43336adca85919711",
+    "url": "/efrederick/static/media/Nunito-Regular.2759fcd4.ttf"
+  },
+  {
+    "revision": "67acba05f62293ca82d556e52543b3ff",
+    "url": "/efrederick/static/media/Nunito-Regular.67acba05.woff"
+  },
+  {
+    "revision": "834d6b883f94a6fb246bb6e3a7949579",
+    "url": "/efrederick/static/media/Nunito-Regular.834d6b88.svg"
+  },
+  {
+    "revision": "3be88c657bb7ac49314d04a8d9f79912",
+    "url": "/efrederick/static/media/Nunito-SemiBold.3be88c65.ttf"
+  },
+  {
+    "revision": "564323126ecd82adbee3fb6b6470c218",
+    "url": "/efrederick/static/media/Nunito-SemiBold.56432312.eot"
+  },
+  {
+    "revision": "95c5923aa9c48a33ec9026a4c99ceb60",
+    "url": "/efrederick/static/media/Nunito-SemiBold.95c5923a.woff"
+  },
+  {
+    "revision": "b4ee600d3b6ea85aaa7ade03fd24cc53",
+    "url": "/efrederick/static/media/Nunito-SemiBold.b4ee600d.svg"
+  },
+  {
+    "revision": "3c093190f92b5371519631d1ce9c1064",
+    "url": "/efrederick/static/media/about.3c093190.svg"
+  },
+  {
+    "revision": "84cddfd35761b2dbb9df4e1275933fad",
+    "url": "/efrederick/static/media/contact.84cddfd3.svg"
+  }
+]);
