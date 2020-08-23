e164cc3b9c7d2d89f840cb8a3a855a06a4568d37
pepeyen <erickfcaldeira@gmail.com>
2020-08-23 18:47:41 -0300

    Quality of life improvement by language based redirection

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,45 @@
+import React from 'react'
+import {useSelector} from 'react-redux';
+
+//Components
+import LangSelector from '../LangSelector';
+
+//Styles
+import './footer.scss';
+
+function Footer() {
+    const currentPageLanguage = useSelector(state => state.pageLanguage);
+
+    return (
+        <div className="footer">
+            <div className="footer__text">
+                <div className="footer__logo">
+                    <p>eFrederick</p>
+                </div>
+                <div className="footer__social">
+                    <div className="footer__social-link --spaced-from-top">
+                    <a 
+                        className="footer__outside-link" 
+                        href={`https://www.linkedin.com/in/erick-frederick-c/?locale=${currentPageLanguage}`}
+                        target="_blank" rel="noopener noreferrer"
+                    >
+                        Linkedin
+                    </a>
+                    </div>
+                    <div className="footer__social-link --spaced-from-top">
+                    <a 
+                        className="footer__outside-link" 
+                        href="https://github.com/pepeyen" 
+                        target="_blank" rel="noopener noreferrer"
+                    >
+                        Github
+                    </a>
+                    </div>
+                </div>
+                <LangSelector />
+            </div>
+        </div>
+    );
+}
+
+export default Footer;
