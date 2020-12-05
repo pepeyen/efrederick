4edc4f6f84708e52dbea921be3a3165c77a53f4f
pepeyen <erickfcaldeira@gmail.com>
2020-12-05 02:53:33 -0300

    Follow up to repo updates

--- src/services/mockUpData.js
+++ src/services/mockUpData.js
@@ -1,6 +1,6 @@
 export const projectList = [
     {projectName: 'cs-tech', projectLiveURL: 'https://pepeyen.github.io/cs-tech', projectBannerURL: 'https://repository-images.githubusercontent.com/245696462/5bab8980-cf60-11ea-95f0-bc5f566cd9e8'},
     {projectName: 'efrederick-cli', projectLiveURL: 'https://pepeyen.github.io/efrederick-cli', projectBannerURL: 'https://repository-images.githubusercontent.com/275693390/6b759e80-cf5d-11ea-8d8f-a22f2ac03276'},
-    {projectName: 'namah-front-end', projectLiveURL: 'https://pepeyen.github.io/namah-front-end', projectBannerURL: 'https://repository-images.githubusercontent.com/293850098/ea1a7980-3278-11eb-9f1a-3ff1f4cf768d'}, 
-    {projectName: 'namah-back-end', projectLiveURL: 'https://project-namah.herokuapp.com', projectBannerURL: 'https://repository-images.githubusercontent.com/307787791/e805ea00-327b-11eb-9a0c-e060b06e9c28'}
+    {projectName: 'namah', projectLiveURL: 'https://pepeyen.github.io/namah', projectBannerURL: 'https://repository-images.githubusercontent.com/293850098/ea1a7980-3278-11eb-9f1a-3ff1f4cf768d'}, 
+    {projectName: 'portfolio-api', projectLiveURL: 'https://efrederick-back-end.herokuapp.com/namah', projectBannerURL: 'https://repository-images.githubusercontent.com/307787791/e805ea00-327b-11eb-9a0c-e060b06e9c28'}
 ];
