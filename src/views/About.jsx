78705302d0f5dc74f121c52bc8648046c4874dcd
pepeyen <erickfcaldeira@gmail.com>
2021-01-04 19:54:16 -0300

    Follow up to the portfolio endpoint update

--- src/views/About.jsx
+++ src/views/About.jsx
@@ -10,9 +10,8 @@ import {
 //Innerpage Routing
 import {
 	updateScreenPosition,
-	getRepoList,
-	getRepo,
-	animateValue
+	animateValue,
+	fetchFromBackEnd
 } from '../services'
 
 //Language library
@@ -31,86 +30,19 @@ const About = () => {
 	useEffect(() => {
 		setIsLoading(true);
 
-		fetch(`${process.env.REACT_APP_BACK_END_HOST}/api/v1/keys`, {
-			headers: {
-				'Content-Type': 'application/json'
-			},
-			method: 'POST',
-			body: JSON.stringify({
-				keyId: 'ghub:1',
-				keyOrigin: 'github'
-			})
-		})
-		.then(response => {
-			return response.json();
-		})
+		fetchFromBackEnd('projects', null, {method: 'GET'})
 		.then(data => {
-			return data.key;
-		})
-		.then(key => {
-			if(key.api_key_value){
-				const myHeaders = new Headers();
+			if(data.success){
+				const totalLinesOfCode = data.projects.totalLOC, totalNumberOfRepos = data.projects.count;
 
-				myHeaders.append('authorization', `token ${key.api_key_value}`);
+				setIsLoading(false);
 
-				getRepoList('pepeyen', myHeaders)
-				.then(data => {
-					let projectList = [];
-
-					data.forEach(repository => {
-						if(repository.homepage && repository.name !== 'efrederick'){
-							projectList.push({
-								repositoryName: repository.name,
-								reporitoryThumbnailURL: `https://raw.githubusercontent.com/${repository.owner.login}/${repository.name}/master/.github/images/project-thumbnail.png`,
-								repositoryURL: repository.html_url,
-								repositoryDemoURL: repository.homepage
-							});
-						}
-					});
-
-					dispatch(setProjectList(projectList));
-
-					return data;
-				})
-				.then(data => {
-					return data.map(currentRepo => getRepo('pepeyen', currentRepo.name, myHeaders)
-					.then(contributors => contributors
-						.map(contributor => contributor.weeks
-							.reduce((lineCount, week) => lineCount + week.a - week.d, 0)
-						)
-					)
-					.then(lineCounts => {
-						return lineCounts.reduce((lineTotal, lineCount) => {
-							return lineTotal + lineCount;
-						});
-					})
-					.then(lines => {
-						return lines;
-					}))
-				})
-				.then(result => {
-					return Promise.all(result);
-				})
-				.then(linesOfCodeList => {
-					let totalLinesOfCode = 0, totalNumberOfRepos = 0;
-
-					linesOfCodeList.map(currentRepoLinesOfCode => {
-						if(currentRepoLinesOfCode >= 300){
-							totalLinesOfCode = totalLinesOfCode + Math.round(currentRepoLinesOfCode * 0.01);
-						}else{
-							totalLinesOfCode = totalLinesOfCode + currentRepoLinesOfCode;
-						}
-						
-						return null;
-					})
-					totalNumberOfRepos = linesOfCodeList.length;
-
-					setIsLoading(false);
-					setLinesOfCode({totalLinesOfCode, totalNumberOfRepos});
-
-					animateValue("lines-of-code", Math.round(totalLinesOfCode * 0.9), totalLinesOfCode, 340);
-					animateValue("total-repos", Math.round(totalNumberOfRepos * 0.2), totalNumberOfRepos, 340);
-				})
+				setLinesOfCode({totalLinesOfCode, totalNumberOfRepos});
+	
+				animateValue("lines-of-code", Math.round(totalLinesOfCode * 0.99), totalLinesOfCode, 340);
+				animateValue("total-repos", Math.round(totalNumberOfRepos * 0.2), totalNumberOfRepos, 340);
+	
+				dispatch(setProjectList(data.projects.list));
 			}
 		})
 	},[dispatch]);
